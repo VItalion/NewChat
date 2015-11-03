efa5dc8adea0340aa6ecaf9093fba4bc5e960a31
Виталий <witalxp@gmail.com>
2015-11-04 01:38:31 +0200

    Добавлен каркас WCF.

---
+++ ChatServiceLibrary/ChatService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ChatServiceLibrary
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "Service1" в коде и файле конфигурации.
+    public class ChatService : IChatService
+    {
+        public bool CreateRoom(string name, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Inside(string roomName, string roomPwd, string userLogin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Leave(string roomName, string userLogin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogOut(string login)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendMessage(string sender, string recipient, string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SignIn(string login, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SignUp(string login, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ChatServiceLibrary/IChatService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ChatServiceLibrary
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени интерфейса "IService1" в коде и файле конфигурации.
+    [ServiceContract(CallbackContract = typeof(IChatServiceCallback))]
+    public interface IChatService
+    {
+        [OperationContract]
+        bool SignUp(string login, string password);
+
+        [OperationContract]
+        bool SignIn(string login, string password);
+
+        [OperationContract(IsOneWay = true)]
+        void SendMessage(string sender, string recipient, string message);
+
+        [OperationContract]
+        void LogOut(string login);
+
+        [OperationContract]
+        bool CreateRoom(string name, string password);
+
+        [OperationContract]
+        bool Inside(string roomName, string roomPwd, string userLogin);
+
+        [OperationContract]
+        void Leave(string roomName, string userLogin);
+    }
+}
